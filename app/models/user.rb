9dacb594177b5c6dd01652071e7f88c67f12a18d
Christian Schimetschka <christian.schimetschka@doctolib.com>
2023-11-02 17:57:43 +0100

    new: add first unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+    message: 'Please provide valid email address'
+  }
   validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'name validation' do
+    user = User.new(:email => 'test@test.de')
+
+    assert_not user.valid?
+  end
 end
