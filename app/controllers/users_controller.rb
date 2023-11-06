d7e3e6fd6150d083bd3814ff4ee9db732358a3a9
Christian Schimetschka <christian.schimetschka@doctolib.com>
2023-11-06 22:50:57 +0100

    new: index and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def create
-    @user = User.create!(params)
+    @user = User.create!(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
   end
 end
