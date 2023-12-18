93f22838f54c105fae5f3b4764a9ab81bd3dd802
Christian Schimetschka <christian.schimetschka@doctolib.com>
2023-12-18 17:35:50 +0100

    format: indentation

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,9 +2,9 @@ class Match < ApplicationRecord
   has_many :user_matches
   has_many :users, through: :user_matches
   
-    def create_user_matches(users)
-      users.map do |user|
-        UserMatch.create!(match: self, user: user)
-      end
+  def create_user_matches(users)
+    users.map do |user|
+      UserMatch.create!(match: self, user: user)
     end
+  end
 end
