1ec01fee38fa2bd4ce361123ada086c21b2ce2e6
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-01-14 19:06:29 +0100

    new: add current_user method to application_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     return render json: "Invalid password", status: :bad_request unless user.authenticate(params[:password])
 
-    session[:user_id] = user.id
+    session[:current_user_id] = user.id
     render json: "Session created", status: :created
 
   rescue Exception
