b46bba1490aff5d888ee34ff6466e3780e86b60c
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-05-08 15:19:15 +0200

    chore: controller + test shell

---
+++ test/controllers/password_resets_controller_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
