fd55571a8749fce14d6861b2100a96de6d8bcfd8
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-07-18 15:24:11 +0200

    chore: config

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,4 +1,5 @@
 import type { Component } from 'solid-js';
+import './App.css'
 
 const App: Component = () => {
   return (
--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -1,5 +1,5 @@
 import { render } from 'solid-js/web';
 import 'virtual:uno.css'
-import App from '../components/App';
+import App from '@/components/App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
