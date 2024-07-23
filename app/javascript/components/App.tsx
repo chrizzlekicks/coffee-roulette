f0ac571d5bf14367052a10f2a8d158a843f2a946
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-07-23 10:20:08 +0200

    feat: implement simple form logic

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,45 @@
-import type { Component } from 'solid-js';
+import { type Component, createSignal } from 'solid-js';
 import './App.css'
 
 const App: Component = () => {
+  const [username, setUsername] = createSignal('')
+  const [email, setEmail] = createSignal('')
+  const [password, setPassword] = createSignal('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const formData = new FormData()
+
+    formData.append('username', username())
+    formData.append('email', email())
+    formData.append('password', password())
+
+    return fetch('/users', { method: 'POST', body: formData })
+  }
+
   return (
-      <h1>Hello World!!!</h1>
+      <>
+        <header class='flex justify-center p-2'>
+          <h1 class='color-sky'>Welcome to Coffee Roulette</h1>
+        </header>
+        <form>
+          <h2>Signup</h2>
+          <div>
+            <label>Username: </label>
+            <input name='username' type='text' onChange={(e) => setUsername(e.target.value)} value={username()} />
+          </div>
+          <div>
+            <label>E-Mail address: </label>
+            <input name='email' type='email' onChange={(e) => setEmail(e.target.value)} value={email()} />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input name='password' type='password' onChange={(e) => setPassword(e.target.value)} value={password()} />
+          </div>
+          <button type='submit' onClick={handleSubmit}>Submit</button>
+        </form>
+      </>
   );
 };
 
