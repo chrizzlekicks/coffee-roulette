66537f7925586197b571a439da854beec1484eca
chrizzlekicks <chris.schimetschka@gmail.com>
2024-07-24 19:05:48 +0200

    chore: remove uno css

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import solidPlugin from 'vite-plugin-solid'
-import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    UnoCSS(),
     solidPlugin(),
   ],
   build: {
