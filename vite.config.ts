37c26eb9946c94f6288a95cde37a556c92dd1ca9
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-08-07 15:24:55 +0200

    chore: remove eslint plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
 import solidPlugin from 'vite-plugin-solid';
-import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     solidPlugin(),
-    eslintPlugin(),
   ],
   build: {
     target: 'esnext',
