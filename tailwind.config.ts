82cb2779f3ad2e57a138f53d624a926b9b3d2fd8
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-08-21 12:11:29 +0200

    style: tailwind config with new theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,22 @@ export default {
     './app/javascript/**/*.{js,ts,jsx,tsx}',
     './app/views/**/*',
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        coffeeroulette: {
+            "primary": "#0ea5e9",
+            "secondary": "#7dd3fc",
+            "accent": "#0369a1",
+            "neutral": "#e0f2fe",
+            "base-100": "#f3f4f6",
+            "info": "#fef9c3",
+            "success": "#00ff00",
+            "warning": "#f59e0b",
+            "error": "#ff0000",
+        }
+      }
+    ]
   },
   plugins: [
     daisyui,
