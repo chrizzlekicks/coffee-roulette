45c91cfc25f45a8e4d2b4b60e718930b72ff8b59
Christian Schimetschka <christian.schimetschka@doctolib.com>
2024-08-27 19:23:55 +0200

    feat: build component for Navbar, style Home and Signin pages

--- app/javascript/pages/Signin.tsx
+++ app/javascript/pages/Signin.tsx
@@ -1,7 +1,7 @@
 import { createSignal } from 'solid-js';
 import httpClient from '../lib/httpClient.ts';
 import createPayload from '../lib/createPayload.ts';
-import {A} from "@solidjs/router";
+import Navbar from "../components/Navbar";
 
 interface User {
     username?: string,
@@ -11,50 +11,50 @@ interface User {
 }
 
 const initialPayload: User = {
-  username: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
 };
 
 const Signin = () => {
-  const [user, setUser] = createSignal(initialPayload);
-  const [message, setMessage] = createSignal();
+    const [user, setUser] = createSignal(initialPayload);
+    const [message, setMessage] = createSignal();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    return httpClient.post('/users', createPayload(user(), 'user')).then((data) => {
-      setMessage(`The user ${data.username} was created successfully`);
-      setUser(initialPayload);
-    }).catch((error: Error) => {
-      setMessage(error.message);
-    }).finally(() => setTimeout(() => setMessage(), 3000));
-  };
+        return httpClient.post('/users', createPayload(user(), 'user')).then((data) => {
+            setMessage(`The user ${data.username} was created successfully`);
+            setUser(initialPayload);
+        }).catch((error: Error) => {
+            setMessage(error.message);
+        }).finally(() => setTimeout(() => setMessage(), 3000));
+    };
 
-  return (
-      <>
-          <nav>
-              <A href='/'>Home</A>
-              <A href='/signin'>Signin</A>
-          </nav>
-        <header class='flex justify-center p-2'>
-          <h1 class='text-4xl'>Welcome to Coffee Roulette</h1>
-        </header>
-        <div>{message()}</div>
-        <form class='form-control'>
-          <label for='username' class='label'>Username: </label>
-          <input class='input input-bordered' name='username' type='text' onChange={(e) => setUser({ ...user(), username: e.target.value })} value={user().username} />
-          <label for='email' class='label'>E-Mail address: </label>
-          <input class='input input-bordered' name='email' type='email' onChange={(e) => setUser({ ...user(), email: e.target.value })} value={user().email} />
-          <label for='password' class='label'>Password: </label>
-          <input class='input input-bordered' name='password' type='password' onChange={(e) => setUser({ ...user(), password: e.target.value })} value={user().password} />
-          <label for='password_confirmation' class='label'>Confirm password: </label>
-          <input class='input input-bordered' name='password_confirmation' type='password' onChange={(e) => setUser({ ...user(), confirmPassword: e.target.value })} value={user().confirmPassword} />
-          <button class='btn btn-primary' type='submit' onClick={handleSubmit}>Submit</button>
-        </form>
-      </>
-  );
+    return (
+        <>
+            <Navbar />
+            <section class="flex justify-center items-center min-h-screen bg-base-200">
+                <div class="card bg-base-100 text-primary-content w-96">
+                    <div class="card-body">
+                        <h2 class="card-title">Signup with us!</h2>
+                        <form class='form-control'>
+                            <label for='username' class='label'>Username: </label>
+                            <input class='input input-bordered' name='username' type='text' onChange={(e) => setUser({ ...user(), username: e.target.value })} value={user().username} />
+                            <label for='email' class='label'>E-Mail address: </label>
+                            <input class='input input-bordered' name='email' type='email' onChange={(e) => setUser({ ...user(), email: e.target.value })} value={user().email} />
+                            <label for='password' class='label'>Password: </label>
+                            <input class='input input-bordered' name='password' type='password' onChange={(e) => setUser({ ...user(), password: e.target.value })} value={user().password} />
+                            <label for='password_confirmation' class='label'>Confirm password: </label>
+                            <input class='input input-bordered' name='password_confirmation' type='password' onChange={(e) => setUser({ ...user(), confirmPassword: e.target.value })} value={user().confirmPassword} />
+                            <button class='btn btn-primary mt-4' type='submit' onClick={handleSubmit}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Signin;
