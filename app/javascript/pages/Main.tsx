bce225e4506fe87b7584028727f2b7267ba837e5
Christian Schimetschka <christian.schimetschka@doctolib.com>
2025-04-09 18:19:20 +0200

    feat: add simple data fetching for matches

--- app/javascript/pages/Main.tsx
+++ app/javascript/pages/Main.tsx
@@ -1,9 +1,31 @@
 import Layout from "../components/Layout";
+import httpClient from "../lib/httpClient";
+import {createResource, Show, Switch, Match, For} from "solid-js";
 
-const Main = () => (
-	<Layout>
-		<h1>Welcome</h1>
-	</Layout>
-);
+export const getMatches = () => httpClient.get('/matches').then((data: any) => data)
+
+const Main = () => {
+	const [matches] = createResource(getMatches)
+
+	return (
+		<Layout>
+			<Show when={matches.loading}>
+				<h1>Welcome</h1>
+			</Show>
+			<Switch>
+				<Match when={matches.error}>
+					<span>Error: {matches.error}</span>
+				</Match>
+				<Match when={matches()}>
+					<ol>
+						<For each={matches()}>
+							{(match)=> <li>{match.date}</li>}
+						</For>
+					</ol>
+				</Match>
+			</Switch>
+		</Layout>
+	);
+}
 
 export default Main;
